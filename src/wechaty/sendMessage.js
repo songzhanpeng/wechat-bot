092d03aaa3d2a1d0a261d2600cb893281fc91a4b
songzhanpeng <songzhanpeng@iauto.com>
2024-02-01 09:46:21 +0800

    feat: 修

--- src/wechaty/sendMessage.js
+++ src/wechaty/sendMessage.js
@@ -41,7 +41,7 @@ export async function defaultMessage(msg, bot) {
     try {
       // 区分群聊和私聊
       if (isRoom && room) {
-       const mention = room.mention()
+       const mention = msg.mention()
         console.log("🚀 ~ defaultMessage ~ mention:", mention)
         const member = await room.member(name) // all members in this room
         console.log("🚀 ~ defaultMessage ~ member:", member)
