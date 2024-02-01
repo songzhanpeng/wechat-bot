6d6ac1fdaf8bac49d5c47abdad812d2dda58c879
songzhanpeng <songzhanpeng@iauto.com>
2024-02-01 09:58:50 +0800

    feat: 优化

--- src/wechaty/sendMessage.js
+++ src/wechaty/sendMessage.js
@@ -21,31 +21,31 @@ export async function defaultMessage(msg, bot) {
   const isAlias = aliasWhiteList.includes(remarkName) || aliasWhiteList.includes(name) || aliasWhiteList.includes('*'); // 发消息的人是否在联系人白名单内
   const isBotSelf = botName === remarkName || botName === name; // 是否是机器人自己
 
-  
+
   //  console.log('接收到消息类型：', bot.Message.Type[msg.type()]);
-  
+
   // 如果消息类型为文本且不是机器人自己发送的消息
   if (isText && !isBotSelf) {
     console.log(JSON.stringify(msg));
-    
+
     // 检查消息时间戳，如果距离现在超过10秒则不处理
     const messageTimestamp = 1000 * msg.payload.timestamp;
     const currentTimestamp = Date.now();
     const timeDifference = currentTimestamp - messageTimestamp;
-    
+
     if (timeDifference > 10 * 1000) {
       console.log(`消息时间戳超过10秒，当前时间戳: ${currentTimestamp}, 消息时间戳: ${messageTimestamp}`);
       return;
     }
-    
+
     try {
       // 区分群聊和私聊
       if (isRoom && room) {
-       const mention = await msg.mention();
-       const mentionList = mention.map(m => m.id);
+        const mention = await msg.mention();
+        const mentionList = mention.map(m => m.id);
         console.log("🚀 ~ defaultMessage ~ mentionList:", mentionList)
         // 在群聊中回复消息
-        await room.say(await getReply(content.replace(`@${botName}`, '')), mentionList);
+        await room.say(await getReply(content.replace(`@${botName}`, '')), { mentionList });
         return;
       }
 
