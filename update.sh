b1435926b2393217e22cc278268cedefeea26402
songzhanpeng <songzhanpeng@iauto.com>
2024-03-28 16:46:59 +0800

    feat: 优化了更新脚本

--- update.sh
+++ update.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 # 执行 git pull 命令
-echo -e "\e[1;34m🔄 Executing \e[1;33mgit pull\e[0m..."
+echo -e "\e[1;34m🔄 正在更新代码库...\e[0m"
 git pull
 
-# 检查是否成功更新代码
+# 检查更新是否成功
 if [ $? -ne 0 ]; then
-    echo -e "\e[1;31m❌ Git pull failed! Please check for code updates.\e[0m"
+    echo -e "\e[1;31m❌ 更新失败！请检查代码库。\e[0m"
     exit 1
 fi
 
 # 执行 npm install 命令
-echo -e "\e[1;34m📦 Installing dependencies with \e[1;33mnpm install\e[0m..."
+echo -e "\e[1;34m📦 正在安装依赖项...\e[0m"
 npm install
 
-# 执行 pm2 stop 命令
-echo -e "\e[1;34m⏹️ Stopping \e[1;33mwechat-bot\e[0m..."
+# 执行 pm2 命令
+echo -e "\e[1;34m⏹️ 正在停止 wechat-bot...\e[0m"
 pm2 stop wechat-bot
 
-# 执行 pm2 start 命令
-echo -e "\e[1;34m▶️ Starting \e[1;33mwechat-bot\e[0m..."
+# 执行 pm2 命令
+echo -e "\e[1;34m▶️ 正在启动 wechat-bot...\e[0m"
 pm2 start index.js --name=wechat-bot
 
-# 检查是否成功启动应用程序
+# 检查启动是否成功
 if [ $? -ne 0 ]; then
-    echo -e "\e[1;31m❌ Failed to start \e[1;33mwechat-bot\e[0m.\e[0m"
+    echo -e "\e[1;31m❌ 启动失败！\e[0m"
     exit 1
 fi
 
-echo -e "\e[1;32m✅ \e[1;33mwechat-bot\e[1;32m started successfully!\e[0m"
+echo -e "\e[1;32m✅ wechat-bot 成功启动！\e[0m"
